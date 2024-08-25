6d67cd18c80dee3a58dfb43371f653d6587c8333
”slukashenko” <slukashenko75@gmail.com>
2024-08-25 17:20:28 +0500

    Refactor ManagerSaveException

--- src/ru/yandex/kanban/manager/ManagerSaveException.java
+++ src/ru/yandex/kanban/manager/ManagerSaveException.java
@@ -2,7 +2,11 @@ package ru.yandex.kanban.manager;
 
 public class ManagerSaveException extends RuntimeException {
 
-    public ManagerSaveException(String message, Exception exception) {
-        super(message, exception);
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+
+    public ManagerSaveException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
